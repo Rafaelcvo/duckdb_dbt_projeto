d1838576375252bfb0eec6033f6199b5e6f4ccdc
Rafael Moreira <rafaelmoreiracvo@gmail.com>
2025-06-27 13:11:38 -0300

    Test total_vendas

--- src/busca_dados.py
+++ src/busca_dados.py
@@ -1,3 +1,6 @@
 import duckdb
+
 con = duckdb.connect('vendas.duckdb')
-print(con.execute("SELECT * FROM main.vendas_limpas").fetchdf())
+# print(con.execute("SELECT * FROM main.vendas_limpas").fetchdf())
+
+print(con.execute("SELECT * FROM main.total_vendas_por_cliente").fetchdf())
