fa7103b2090d66c1bb1c9ebf32171771b8247f0e
Rafael Moreira <rafaelmoreiracvo@gmail.com>
2025-06-27 13:53:20 -0300

    consultation completed successfully

--- src/busca_dados.py
+++ src/busca_dados.py
@@ -4,3 +4,5 @@ con = duckdb.connect('vendas.duckdb')
 # print(con.execute("SELECT * FROM main.vendas_limpas").fetchdf())
 
 print(con.execute("SELECT * FROM main.total_vendas_por_cliente").fetchdf())
+
+print(con.execute("SELECT * FROM main.vendas_raw").fetchdf())
